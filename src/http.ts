946bfc1e1cf30798370c6e6ab37237af8032c518
sdjdd <i@sdjdd.com>
2022-04-12 17:40:22 +0800

    feat: make http error friendly

--- src/http.ts
+++ src/http.ts
@@ -1,3 +1,15 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
 export const client = axios.create();
+
+client.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  (error: AxiosError<{ message?: string }>) => {
+    if (error.response?.data?.message) {
+      error.message = error.response.data.message;
+    }
+    throw error;
+  }
+);
